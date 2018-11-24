d11e4bbfa6fd5ab0a4280041e5682e82b0a6dd35
John C. Lavelle <jlavelle604@gmail.com>
2018-11-23 21:33:33 -0500

    Refactor Day1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,34 +1,28 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Day1 where
 
 import Control.Lens (ifoldMap)
 import Data.Monoid (Sum(..))
 import Data.List (unfoldr)
 
-digits :: Integer -> [Integer]
+digits :: Integral a => a -> [a]
 digits = reverse . unfoldr go
  where
   go 0 = Nothing
   go i = Just (i `mod` 10, i `div` 10)
 
-solve1 :: Integer -> Integer
-solve1 i = getSum $ ifoldMap go ds
+gsolve :: Integral a => ([a] -> Int -> a) -> a -> a
+gsolve f (digits -> ds) = getSum $ ifoldMap go ds
  where
-  ds = digits i
-  go k a | cd !! succ k == a = Sum a
-         | otherwise         = Sum 0
-   where
-    cd = cycle ds
+  go k a | f ds k == a = Sum a
+         | otherwise   = Sum 0
 
-solve2 :: Integer -> Integer
-solve2 i = getSum $ ifoldMap go ds
- where
-  ds = digits i
-  go k a | cd !! x == a = Sum a
-         | otherwise    = Sum 0
-   where
-    x  = k + div l 2
-    cd = cycle ds
-    l  = length ds
+solve1 :: Integral a => a -> a
+solve1 = gsolve (\as i -> cycle as !! succ i)
+
+solve2 :: Integral a => a -> a
+solve2 = gsolve (\as i -> cycle as !! (i + div (length as) 2))
 
 tests :: [Bool]
 tests =
